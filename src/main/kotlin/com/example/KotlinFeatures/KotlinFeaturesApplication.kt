e60892596e46613826882e040ccb03e7c2167c7e
thekoko711 <avasir2slj@gmail.com>
2024-05-04 20:42:13 +0200

    First commit

---
+++ src/main/kotlin/com/example/KotlinFeatures/KotlinFeaturesApplication.kt
@@ -0,0 +1,11 @@
+package com.example.KotlinFeatures
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinFeaturesApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinFeaturesApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/KotlinFeatures/KotlinFeaturesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.KotlinFeatures
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinFeaturesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
